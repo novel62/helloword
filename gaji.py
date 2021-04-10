867dbd58cb20bc8a5b57856dbb127e9eca619261
novel62 <56954701+novel62@users.noreply.github.com>
2021-04-10 21:04:41 +0700

    menghapus program tanpa fungsi

--- gaji.py
+++ gaji.py
@@ -1,11 +1,4 @@
-gaji1=int(input("masukan gaji pokok bulan :"))
-gaji_thn=12*gaji
-if gaji_thn>= 15600000:
-  ket1='kena pajak'
-else:
-  ket1='tidak kena pajak'
- print('keterangannya adalah:',ket1)
-
+# menghapus statemen program tanpa fungsi
 def hitGaji(g):
   gaji=12*gaji
   print('gaji adalah:',gaji)
