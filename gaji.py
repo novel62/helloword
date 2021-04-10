ad31ca5bfd47a7b5dbf164c2ed662de98ce22871
novel62 <56954701+novel62@users.noreply.github.com>
2021-04-10 20:50:01 +0700

    penambahan memakai fungsi

--- gaji.py
+++ gaji.py
@@ -1,7 +1,17 @@
-gaji=int(input("masukan gaji pokok bulan :"))
+gaji1=int(input("masukan gaji pokok bulan :"))
 gaji_thn=12*gaji
 if gaji_thn>= 15600000:
-  ket='kena pajak'
- else:
-  ket='tidak kena pajak'
- print('keterangannya adalah:',ket)
+  ket1='kena pajak'
+else:
+  ket1='tidak kena pajak'
+ print('keterangannya adalah:',ket1)
+
+def hitGaji(g):
+  gaji=12*gaji
+  print('gaji adalah:',gaji)
+  if gaji >=15600000:
+    ket='kena pajak'
+  else:
+    ket='Tidak kena pajak'
+  print('keterangannya adl =',ket)
+ hitGaji(2000000)
